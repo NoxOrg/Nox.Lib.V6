8df20972e69e5de3234488b8f9f56eb817b0316c
jan <janschutte.lands@gmail.com>
2022-12-05 11:08:04 +0200

    Removed sensitive info from design removed folder requirement from ConfigurationExtensions.cs

--- src/Nox.Core/Configuration/ConfigurationExtensions.cs
+++ src/Nox.Core/Configuration/ConfigurationExtensions.cs
@@ -10,7 +10,6 @@ public static class ConfigurationExtensions
     {
         if (string.IsNullOrEmpty(designRoot)) throw new ConfigurationException("Nox 'DesignRootPath setting may not be empty! Please verify you appsettings or arguments.'");
         if (Directory.GetFiles(designRoot, "*.yaml").Length == 0) throw new ConfigurationException($"Could not find any yaml files in {designRoot}");
-        if (Directory.GetDirectories(designRoot).Length == 0) throw new ConfigurationException($"Could not find any entity folders in {designRoot}");
         var configurator = new NoxConfigurator(designRoot);
         var config = configurator.LoadConfiguration();        
         services.AddSingleton<INoxConfiguration>(config);
