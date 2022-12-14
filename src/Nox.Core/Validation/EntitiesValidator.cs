59b98b36d0bb9a21dacc09a6c7d7bc1b7f4cda1b
jan <janschutte.lands@gmail.com>
2022-12-14 12:06:11 +0200

    Post merge

--- src/Nox.Core/Validation/EntitiesValidator.cs
+++ src/Nox.Core/Validation/EntitiesValidator.cs
@@ -5,5 +5,15 @@ namespace Nox.Core.Validation;
 
 public class EntitiesValidator: AbstractValidator<List<IEntity>>
 {
-    
+    public EntitiesValidator()
+    {
+        RuleForEach(entities => entities)
+            .SetValidator(new EntityValidator());
+        
+    }
+
+    private bool NotBeDuplicate(IEntity entity)
+    {
+        return false;
+    }
 }
