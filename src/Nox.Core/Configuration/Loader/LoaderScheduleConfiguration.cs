ec2800f87d2bab525da1e4a6982b8b5d216957d2
jan <janschutte.lands@gmail.com>
2023-03-03 15:10:32 +0200

    - implemented Loader Schedule RunOnStartup property in Nox configuration

--- src/Nox.Core/Configuration/Loader/LoaderScheduleConfiguration.cs
+++ src/Nox.Core/Configuration/Loader/LoaderScheduleConfiguration.cs
@@ -7,4 +7,5 @@ public class LoaderScheduleConfiguration: MetaBase
     public string Start { get; set; } = string.Empty;
     public string CronExpression { get; set; } = string.Empty;
     public LoaderScheduleRetryPolicyConfiguration? Retry { get; set; } = new();
+    public bool? RunOnStartup { get; set; } = true;
 }
