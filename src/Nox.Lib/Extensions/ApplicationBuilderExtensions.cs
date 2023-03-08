cfc850b33a3a0bb1a627b36d8d2c3ac79f732f9c
Dmytro Dorodnykh <Dmytro.Dorodnykh@iwgplc.com>
2023-03-08 21:33:26 +0200

    move swagger odata filter to be used by default in addNox

--- src/Nox.Lib/Extensions/ApplicationBuilderExtensions.cs
+++ src/Nox.Lib/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,10 @@
 using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nox.Api;
+using Nox.Api.OData.Swagger;
+using Nox.Core.Interfaces;
 using Nox.Core.Interfaces.Configuration;
 
 namespace Nox;
@@ -11,10 +14,22 @@ public static class ApplicationBuilderExtensions
     public static IApplicationBuilder UseNox(
         this IApplicationBuilder builder)
     {
+        builder.UseODataEntitySectionsSwaggerFilter();
+
         if (builder.ApplicationServices.GetService<IProjectConfiguration>() == null) return builder;
         builder.UseMiddleware<DynamicApiMiddleware>().UseRouting();
 
         builder.UseHangfireDashboard("/jobs");
         return builder;
+    }
+
+    public static IApplicationBuilder UseODataEntitySectionsSwaggerFilter(
+        this IApplicationBuilder appBuilder)
+    {
+        ODataEntitySectionsSwaggerFilter.Initialize(
+            appBuilder.ApplicationServices.GetService<ILogger<ODataEntitySectionsSwaggerFilter>>(),
+            appBuilder.ApplicationServices.GetService<IDynamicService>());
+
+        return appBuilder;
     }
 }
