5efa6640714bbdd7e2df8d5b0ec307494fee9aff
Andre Sharpe <andre.sharpe@gmail.com>
2023-04-21 07:13:27 +0200

    App access to IProjectConfiguration for Swagger

---
+++ src/Nox.Lib/Extensions/NoxSwaggerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Nox.Core.Extensions;
+
+namespace Nox
+{
+    public class NoxSwaggerConfiguration
+    {
+        public bool UseSwaggerGen { get; set; } = true;
+        public bool UseODataEntitySectionsSwaggerFilter { get; set; } = true;
+        public Action<SwaggerGenOptions> SetupAction { get; set; } = cfg =>
+        {
+            var projectConfig = ServiceCollectionExtensions.NoxProjectConfiguration;
+
+            cfg.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "0.01",
+                Title = projectConfig?.Name,
+                Description = projectConfig?.Description
+            });
+        };
+    }
+}
